771f3e4ebe5e5a7dd3c6a6cb81bef2441b68d954
Prateek <hridayved07@gmail.com>
2026-07-18 07:39:48 +0000

    Practice Scanner input and addition

---
+++ 01_Basics/OperatorSum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class OperatorSum{
+    public static void main(String[] args){
+        Scanner sc= new Scanner (System.in);
+        System.out.println("enter a and b no.s respectively");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("the sum of a and b is" + sum);
+    }
+}
