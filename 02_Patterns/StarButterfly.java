003e56c515a56112a5dee44663c7aee7b5d25884
Prateek <hridayved07@gmail.com>
2026-08-02 16:15:04 +0000

    Add Star Butterfly pattern program

---
+++ 02_Patterns/StarButterfly.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class StarButterfly{
+    public static void main(String args []){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Butterfly Pattern");
+        System.out.print("Enter number of rows:");
+        int rows = sc.nextInt();
+        for(int i = 1;i <= rows ;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }for (int j=1;j<=(2*(rows-i));j++){
+                System.out.print(" ");
+            }for (int j = 1;j<=i;j++){
+                System.out.print("*");
+            }System.out.println();
+        }for(int i = rows;i >= 1 ;i--){
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }for (int j=1;j<=(2*(rows-i));j++){
+                System.out.print(" ");
+            }for (int j = 1;j<=i;j++){
+                System.out.print("*");
+            }System.out.println();
+        }sc.close();
+    }
+}
