3fd1807f3ad9b41f82436046e881b056a75a6e6f
Prateek <hridayved07@gmail.com>
2026-08-22 13:00:33 +0000

    Add program to print an infinite do while loop.

---
+++ 01_Basics/InfiniteDoWhile.java
@@ -0,0 +1,7 @@
+public class InfiniteDoWhile{
+    public static void main(String args[]){
+        do{
+            System.out.println("An infinte loop use ctrl + c to end.");
+        }while(true);
+    }
+}
